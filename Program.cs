3d2ab28e8ef71e1f9325db310170baa85766af4d
Falcon007 <53327905+ChidvilasBalla@users.noreply.github.com>
2025-02-26 21:18:49 +0530

    Reinstall angular due to issues with ngx-bootstrap

--- Program.cs
+++ Program.cs
@@ -11,10 +11,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     //This would be the connection string
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(); 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
